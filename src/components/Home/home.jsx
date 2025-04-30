18c2a0c93e6e9d3ca5f1ad6a772bbe21a9179988
Conor Wantuch <cwantuch7@gmail.com>
2025-04-30 10:34:45 -0600

    changed the books page

--- src/components/Home/home.jsx
+++ src/components/Home/home.jsx
@@ -8,27 +8,13 @@ const Home = () => {
       <p className="home-paragraph">
         Where all your favorite books and movies live!
       </p>
+      <div>
+        <img src='images/stack-of-books.png'/>
+      </div>
     </div>
   );
 };
 
-
-const styles = {
-  container: {
-    padding: '2rem',
-    textAlign: 'center',
-    backgroundColor: '#f5f5f5',
-    minHeight: '100vh',
-  },
-  heading: {
-    fontSize: '2.5rem',
-    color: '#333',
-  },
-  paragraph: {
-    fontSize: '1.2rem',
-    color: '#555',
-  },
-};
 
 export default Home;
 
