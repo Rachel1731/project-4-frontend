4f821b0942a1ff47a67f3325b28eb5b09307a9c4
Conor Wantuch <cwantuch7@gmail.com>
2025-05-01 14:43:46 -0600

    adding to game.jsx

--- src/components/Game/game.jsx
+++ src/components/Game/game.jsx
@@ -1,4 +1,29 @@
+import React, {useState, useEffect} from 'react';
+
 const Game = () => {
+  const [books, setBooks] = useState([]);
+  const [bookRatings, setBookRatings] = useState({})
+  useEffect(() => {
+    const fetchBooks = async () => {
+      try {
+        const response = await fetch('http://3.83.236.184:8000/api/books/', {
+          method: 'GET',
+        });
+        const booksData = await response.json();
+        setBooks(booksData);
+
+        for (const book of booksData) {
+          const ratingResponse = await fetch(`https://www.googleapis.com/books/v1/volumes?q=intitle:${encodeURIComponent(book.title)}`);
+          const ratingsData = await ratingResponse.json()
+          console.log(ratingsData)
+        }
+      } catch {
+
+      }
+    }
+    fetchBooks();
+  }, [])
+
     return (
       <h1>Game Page</h1>
     )
