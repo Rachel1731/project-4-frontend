76a9ce96c2a0b2257e37bb168db016d5fa0000ec
Conor Wantuch <cwantuch7@gmail.com>
2025-05-05 18:58:13 -0600

    added css to home

--- src/components/Home/home.jsx
+++ src/components/Home/home.jsx
@@ -1,25 +1,47 @@
 import React from 'react';
 import './home.css'; 
+import { motion, AnimatePresence } from "framer-motion";
+
 const Home = () => {
 
   return (
-    
+    <AnimatePresence>
     <div className="home-container">
-      <div className="homeheading">
+      <motion.div 
+      className="homeheading"
+      initial={{opacity: 0, y: -50}}
+      animate={{opacity: 1, y: 0, x: 0}}
+      exit={{opacity: 0, y: -50}}
+      transition={{duration: .5}}
+      >
       <h1>Welcome to Page and Picture</h1>
       <p className="home-paragraph">
-        Where all your favorite books and movies live!
+        Where all your favorite books and movies lived!
       </p> 
-      </div>
-     <div className="container">
-        <div className="movie">
+      </motion.div>
+      
+      <div className="container">
+        <motion.div 
+        className="movie"
+        initial={{opacity: 0, y: 100, x: -90}}
+        animate={{opacity: 1, y: 0, x: 0}}
+        exit={{opacity: 0, y: -50}}
+        transition={{duration: .5}}
+        >
           <img  alt="Movies" src="images/image (1).png" />
-        </div>
-        <div className="book">
+        </motion.div>
+        <motion.div 
+        className="book"
+        initial={{opacity: 0, y: 100, x: 90}}
+        animate={{opacity: 1, y: 0, x: 0}}
+        exit={{opacity: 0, y: -50}}
+        transition={{duration: .5}}
+        >
           <img  alt="Books" src="images/image.png" />
-        </div >
+        </motion.div >
      </div>
     </div>
+    </AnimatePresence>
         
   );
 };
